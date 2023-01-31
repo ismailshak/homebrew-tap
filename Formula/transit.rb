6c23fae52c7c7a25c01c301ef6a3fc3179657531
ismailshak <ismailshak94@yahoo.com>
2023-01-31 17:39:21 -0500

    Use v0.3.0

--- Formula/transit.rb
+++ Formula/transit.rb
@@ -5,20 +5,21 @@ class Transit < Formula
   license "MIT"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/ismailshak/transit/releases/download/v0.2.0/transit_Linux_x86_64.tar.gz"
-    sha256 "39f7d998b1205b3a7b72cfeb18c5a8483cb8602b318f5b1fcac249d3895f3652"
+    url "https://github.com/ismailshak/transit/releases/download/v0.3.0/transit_Linux_x86_64.tar.gz"
+    sha256 "9ecb0e07155794db64700e22ada2acb399925024f2356d505cca36c7eea94a27"
   end
   if OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/ismailshak/transit/releases/download/v0.2.0/transit_Linux_arm64.tar.gz"
-    sha256 "bc6d3c5dcb98be5792e5dec3c4efb087dcf8803f824dc8d08a8bbc0ee8530b36"
+    url "https://github.com/ismailshak/transit/releases/download/v0.3.0/transit_Linux_arm64.tar.gz"
+    sha256 "d3e4fd909e81b1b44838f9944f8697444b9660a03428560006625b7ca0f9ad17"
+  end
+
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/ismailshak/transit/releases/download/v0.3.0/transit_Darwin_x86_64.tar.gz"
+    sha256 "d4599f2f4a115398ec3475345514439d5b2fb270a35da393d03df4879d4eea43"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/ismailshak/transit/releases/download/v0.2.0/transit_Darwin_arm64.tar.gz"
-    sha256 "62f173b11fbaca7c7fa20fa449047a7f94b0c965d08e15e955b12e7ae483bae0"
-  end
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/ismailshak/transit/releases/download/v0.2.0/transit_Darwin_x86_64.tar.gz"
-    sha256 "30da1b37b0f6335262ed9a44802127dcd91156f028c6a6926b2d38cf7333b319"
+    url "https://github.com/ismailshak/transit/releases/download/v0.3.0/transit_Darwin_arm64.tar.gz"
+    sha256 "eb4f044aea41cd5394b69064ec5af8a5c0bb7e5feb699b5a1e8f31c7e6de7fa2"
   end
 
   def install
@@ -26,6 +27,7 @@ class Transit < Formula
   end
 
   test do
-    assert_match "0.2.0", shell_output("#{bin}/transit --version")
+    touch "./config.yml"
+    assert_match "0.3.0", shell_output("#{bin}/transit --version --config ./config.yml")
   end
 end
