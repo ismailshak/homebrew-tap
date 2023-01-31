b75da39ff9a5abe5f3c3d839fe5150b8f5778de3
ismailshak <ismailshak94@yahoo.com>
2023-01-31 18:06:17 -0500

    transit: rebuild
    remove evil dot
    
    Closes #3.
    
    Signed-off-by: Ismail Elshakankiry <23173408+ismailshak@users.noreply.github.com>

--- Formula/transit.rb
+++ Formula/transit.rb
@@ -1,5 +1,5 @@
 class Transit < Formula
-  desc "CLI tool that interacts with local transit information."
+  desc "CLI tool that interacts with local transit information"
   homepage "https://github.com/ismailshak/transit"
   sha256 "62f173b11fbaca7c7fa20fa449047a7f94b0c965d08e15e955b12e7ae483bae0"
   license "MIT"
@@ -27,7 +27,7 @@ class Transit < Formula
   end
 
   test do
-    touch "./config.yml"
-    assert_match "0.3.0", shell_output("#{bin}/transit --version --config ./config.yml")
+    touch "./config.yaml"
+    assert_match "0.3.0", shell_output("#{bin}/transit --version --config ./config.yaml")
   end
 end
