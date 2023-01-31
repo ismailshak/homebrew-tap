06e3d7443587cb6d92b0e8b78f2e94389629fa4d
Ismail Elshakankiry <23173408+ismailshak@users.noreply.github.com>
2023-01-31 23:26:10 +0000

    transit: add 86.64 bottle.

--- Formula/transit.rb
+++ Formula/transit.rb
@@ -4,6 +4,13 @@ class Transit < Formula
   sha256 "62f173b11fbaca7c7fa20fa449047a7f94b0c965d08e15e955b12e7ae483bae0"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/ismailshak/homebrew-tap/releases/download/transit-86.64"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, monterey:     "5e5ce6edcf933f32b71c9105b16e7f4992c3bfa8856c731c2ffc876f50bda468"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "94f448f5a8cf679862f89a2ca47da370bef33c59e4e5e9c3f544290211e81ca5"
+  end
+
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/ismailshak/transit/releases/download/v0.3.0/transit_Linux_x86_64.tar.gz"
     sha256 "9ecb0e07155794db64700e22ada2acb399925024f2356d505cca36c7eea94a27"
