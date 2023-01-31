95ef0317e5db941d113fbcd06f63149bc73467f5
ismailshak <ismailshak94@yahoo.com>
2023-01-31 17:33:50 -0500

    Remove comments

--- Formula/transit.rb
+++ Formula/transit.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Transit < Formula
   desc "CLI tool that interacts with local transit information"
   homepage "https://github.com/ismailshak/transit"
@@ -25,21 +22,10 @@ class Transit < Formula
   end
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    #system "go", "build", *std_go_args(ldflags: "-s -w")
     bin.install "transit"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test transit`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     assert_match "0.2.0", shell_output("#{bin}/transit --version")
   end
 end
