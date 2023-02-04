546803f63474cd122ff43ca4c5dbd9087acb2321
ismailshak <ismailshak94@yahoo.com>
2023-02-04 12:00:58 -0500

    Use version variable

--- Formula/transit.rb
+++ Formula/transit.rb
@@ -5,20 +5,20 @@ class Transit < Formula
   license "MIT"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/ismailshak/transit/releases/download/v0.4.0/transit_Linux_x86_64.tar.gz"
+    url "https://github.com/ismailshak/transit/releases/download/v#{version}/transit_Linux_x86_64.tar.gz"
     sha256 "73cb4655f1622365cfb71a7f882b39efecadaabaee5a00e84eb09f6d35f168ac"
   end
   if OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/ismailshak/transit/releases/download/v0.4.0/transit_Linux_arm64.tar.gz"
+    url "https://github.com/ismailshak/transit/releases/download/v#{version}/transit_Linux_arm64.tar.gz"
     sha256 "a0576ee1248e976f121d4d206b2974e140926bfec5b58e7743d892bcd59b0777"
   end
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/ismailshak/transit/releases/download/v0.4.0/transit_Darwin_x86_64.tar.gz"
+    url "https://github.com/ismailshak/transit/releases/download/v#{version}/transit_Darwin_x86_64.tar.gz"
     sha256 "3a6ac0f01161cabf8226d4651f1931d7f540971b8433a70f34265b2180ad5ee9"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/ismailshak/transit/releases/download/v0.4.0/transit_Darwin_arm64.tar.gz"
+    url "https://github.com/ismailshak/transit/releases/download/v#{version}/transit_Darwin_arm64.tar.gz"
     sha256 "e2d3d9092ea232927c0e1ef5095e6872b23abbdf3c1b7ca79cc2b70e564f4488"
   end
 
@@ -27,6 +27,6 @@ class Transit < Formula
   end
 
   test do
-    assert_match "0.4.0", shell_output("#{bin}/transit --version")
+    assert_match "#{version}", shell_output("#{bin}/transit --version")
   end
 end
