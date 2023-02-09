171aef0ac4ae13c93af113fe6d672c09e4deda03
Ismail Elshakankiry <ielshakankiry@socialtables.com>
2023-02-08 21:34:36 -0500

    transit 1.1.0

--- Formula/transit.rb
+++ Formula/transit.rb
@@ -1,25 +1,25 @@
 class Transit < Formula
   desc "CLI tool that interacts with local transit information"
   homepage "https://github.com/ismailshak/transit"
-  version "1.0.0"
+  version "1.1.0"
   license "MIT"
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/ismailshak/transit/releases/download/v#{version}/transit_Linux_x86_64.tar.gz"
-    sha256 "5ebc6aa1ffe4f62b7934d15ca6683df6c8ca9b9c01de4b10ac47ecfea32bfcd4"
+    sha256 "9f9d8ec61db07e328d5c7d7774d1c4585f36e7382f5e47ce36ab68e7cab3a7de"
   end
   if OS.linux? && Hardware::CPU.arm?
     url "https://github.com/ismailshak/transit/releases/download/v#{version}/transit_Linux_arm64.tar.gz"
-    sha256 "e958171b2a62bb39c4a5af2a84494b2477ae14a0aa9ec25c9c4082e070ef489c"
+    sha256 "9a81fa8352454beb1873d6b52c7bdd4991a6d5145f58a902b61e25d83fd85aad"
   end
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/ismailshak/transit/releases/download/v#{version}/transit_Darwin_x86_64.tar.gz"
-    sha256 "33d32cb1f9915520c5d06a7f1483f09fcbbb3ec4439b523132528296f8249e43"
+    sha256 "d8b8629d1f19f8058335622f040fda1a5c3b148ef49e6dd1b1d948ee049973fe"
   end
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/ismailshak/transit/releases/download/v#{version}/transit_Darwin_arm64.tar.gz"
-    sha256 "8d1fd7e9b67ffcfc9040939fe234ae2cb116221d76c5d48829f2ac05fe7b7546"
+    sha256 "cb04bd254264258983be5ca9a9704aa1b4860ca607cfe29b8563fc645b7376a4"
   end
 
   def install
